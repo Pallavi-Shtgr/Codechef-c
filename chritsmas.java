51e325997f3d405c6ed080db23c37448547ca9ad
Pallavi <pallavishettigar03@gmail.com>
2024-01-02 22:05:14 +0530

    add my first commit

---
+++ chritsmas.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		if(n==25)
+		
+		    System.out.println("CHRISTMAS");
+		
+		else System.out.println("ORDINARY");
+
+	}
+}

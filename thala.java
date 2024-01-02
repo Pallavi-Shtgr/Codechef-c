288c6dc8700661ce0375356c892423ab45087444
Pallavi <pallavishettigar03@gmail.com>
2024-01-02 22:32:45 +0530

    add file via upload

---
+++ thala.java
@@ -0,0 +1,23 @@
+// You are given an integer N.
+//  Print THALA if N=7, otherwise print SADGE.
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class thala
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner in =new Scanner(System.in);
+	    int n=in.nextInt();
+	    	if(n==7)
+		
+		    System.out.println("THALA");
+		
+		else System.out.println("SADGE");
+
+	}
+}
+
+	

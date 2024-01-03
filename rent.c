b499bfc101e4b319a8fe8b68180670f01ecc64b9
Pallavi <pallavishettigar03@gmail.com>
2024-01-03 22:08:47 +0530

    add changes

---
+++ rent.c
@@ -0,0 +1,16 @@
+// Chefina decided to move into Chef's apartment.
+// Chef was initially paying a rent of X rupees. Since Chefina is moving in, the owner decided to double the rent.
+// Find the final rent Chef needs to pay.
+
+#include <stdio.h>
+
+int main() {
+
+	int x;
+	scanf("%d",&x);
+	
+	printf("%d",2*x);
+    }
+
+
+

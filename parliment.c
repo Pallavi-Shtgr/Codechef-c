704949263b8c6b9d3278e37f9258a8585d8a878b
Pallavi <pallavishettigar03@gmail.com>
2024-01-05 21:37:37 +0530

    add question

---
+++ parliment.c
@@ -0,0 +1,5 @@
+An important resolution is being discussed in the Parliament of Chefland. There are 
+N members present in the Parliament out of which 
+X members voted in favour of the resolution and the remaining voted against it.
+According to the constitution of Chefland, a resolution is passed if and only if half or more than half the members present in the Parliament vote in favour of the resolution.
+Determine if the resolution is passed or not.

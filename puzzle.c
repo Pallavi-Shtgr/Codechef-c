e8c38b98155df11250f986c044d7c336d3241547
Pallavi <pallavishettigar03@gmail.com>
2024-01-14 21:55:18 +0530

    add changes

---
+++ puzzle.c
@@ -0,0 +1,18 @@
+// Chef and some of his friends are planning to participate in a puzzle hunt event.
+// The rules of the puzzle hunt state: "This hunt is intended for teams of 6 to 8 people."
+// Chef's team has N people in total. Are they eligible to participate?
+
+#include <stdio.h>
+
+int main() {
+	int i,n;
+	scanf("%d",&n);
+	if(n>=6&& n<=8)
+	{
+	    printf("YES\n");
+	}
+	else
+	
+
+	printf("no\n");
+}

043e2fe1ccfc71fd57c3c8f73ef65058048491ff
Pallavi <pallavishettigar03@gmail.com>
2024-02-07 21:48:15 +0530

    add new files

--- contest.c
+++ contest.c
@@ -1,3 +1,18 @@
 // Chef knows that a starters will be organised on next Wednesday, i.e., Valentine's day.Since it is Starters 120 today, starters 
 // 121 is likely to be organised on Valentine's day.Given an integer 
 // N, find whether starters N is likely to be organised on Valentine's day.
+
+#include <stdio.h>
+
+int main() {
+    int N;
+ 
+    scanf("%d", &N);
+  
+    if (N == 121)
+        printf("Likely\n");
+    else
+        printf("Unlikely\n");
+
+    return 0;
+}

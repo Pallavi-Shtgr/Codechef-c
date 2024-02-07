e84789d092e40a34daad8af859fd875dc5449e93
Pallavi <pallavishettigar03@gmail.com>
2024-02-07 21:47:08 +0530

    add problem statement

---
+++ contest.c
@@ -0,0 +1,3 @@
+// Chef knows that a starters will be organised on next Wednesday, i.e., Valentine's day.Since it is Starters 120 today, starters 
+// 121 is likely to be organised on Valentine's day.Given an integer 
+// N, find whether starters N is likely to be organised on Valentine's day.

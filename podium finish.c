4c8a1562cb32d3fb60507d14b6787039557a7cf8
Pallavi <pallavishettigar03@gmail.com>
2024-02-08 06:43:07 +0530

    add c file

--- podium finish.c
+++ podium finish.c
@@ -4,3 +4,17 @@
 // You are his chief engineer and you only know the time gap between Chef and the runner up of the race, given as 
 // A seconds, and the time gap between the runner up and the winner of the race, given as B seconds.
 // Please calculate and inform Chef about the time gap between him and the winner of the race.
+
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int A,B;
+	    	scanf("%d%d",&A,&B);
+	    	printf("%d\n",A+B); 
+	}
+	return 0;
+}
+

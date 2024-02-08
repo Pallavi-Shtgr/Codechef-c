59e36f76f8c36ed3d78f507b9d7962ab75ecff73
Pallavi <pallavishettigar03@gmail.com>
2024-02-08 06:46:23 +0530

    add new files

---
+++ expiring bread.c
@@ -0,0 +1,3 @@
+// Expiring Bread
+// Eikooc loves bread. She has N loaves of bread, all of which expire after exactly 
+// M days. She can eat upto K loaves of bread in a day. Can she eat all the loaves of bread before they expire?

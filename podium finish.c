38b28d2a146eac5d6f78a3947f91e183cfd829c7
Pallavi <pallavishettigar03@gmail.com>
2024-02-08 06:42:07 +0530

    add problem statement

---
+++ podium finish.c
@@ -0,0 +1,6 @@
+
+// Podium Finish
+// Chef got his dream seat in F1 and secured a 3rdplace in his debut race. He now wants to know the time gap between him and the winner of the race.
+// You are his chief engineer and you only know the time gap between Chef and the runner up of the race, given as 
+// A seconds, and the time gap between the runner up and the winner of the race, given as B seconds.
+// Please calculate and inform Chef about the time gap between him and the winner of the race.

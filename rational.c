e837b76b0ae30acb3ba72657966d9dc94430a522
Pallavi <pallavishettigar03@gmail.com>
2024-02-15 22:54:04 +0530

    add changes

---
+++ rational.c
@@ -0,0 +1,7 @@
+// Input Format
+// The only line of input contains two integers, a and b.
+// Output Format
+// Output a single integer, which is the Height of b
+// a .
+// Constraints
+// 1≤100 , 1≤a,b≤100 a and b do not have any common factors.

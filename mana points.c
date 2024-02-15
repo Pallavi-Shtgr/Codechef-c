7b32d1149a6b2f846db13c1b18e27efa15d29907
Pallavi <pallavishettigar03@gmail.com>
2024-02-15 22:49:25 +0530

    add new changes

---
+++ mana points.c
@@ -0,0 +1,3 @@
+// Mana Points
+// Chef is playing a mobile game. In the game, Chef's character Chefario can perform special attacks. However, one special attack costsX mana points to Chefario.
+// If Chefario currently has Y mana points, determine the maximum number of special attacks he can perform.

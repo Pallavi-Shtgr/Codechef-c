83de9b3e0d60213ff976aa3990373351912b25b0
Pallavi <pallavishettigar03@gmail.com>
2024-02-21 20:34:53 +0530

    add the statement

---
+++ problem review.c
@@ -0,0 +1,6 @@
+// Om Khangat has come up with a problem that he thinks can be used in a CodeChef contest, and has submitted his proposal for review.
+// CodeChef's review panel has N judges, each of whom will give Om's problem a point value between 
+// 1 and 10, denoting how good they think it is (1 being the lowest, and 1010 the highest).A problem is considered good if and only if every judge gives it a score that's strictly greater than 4
+// 4.
+// You know the point values given by each judge to Om's problem. Can you tell whether his problem is good?
+

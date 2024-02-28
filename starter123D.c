7f0409bf88c5184a2e072710f1e35c89cfa7bf59
Pallavi <pallavishettigar03@gmail.com>
2024-02-28 23:01:38 +0530

    add que iwth ans

---
+++ starter123D.c
@@ -0,0 +1,30 @@
+// Algomaniac Finals
+// Algomaniac finals, a part of Convolution Fest of Jadavpur University, will be held on March 17.
+// Shreyan can only go to Jadavpur University on March X.
+// Print YAY if he can attend the Algomaniac finals and NO if he cannot.
+
+// Input Format
+// The first and only line of input contains one integer, X, the day of march Shreyan can go to Jadavpur University.
+
+// Output Format
+// Output YAY if Shreyan can attend Algomaniac finals, and NO otherwise.
+// You may print each character of the string in uppercase or lowercase (for example, the strings YAY, yaY, yay, and Yay will all be treated as identical).
+
+#include <stdio.h>
+
+int main() {
+
+    int x;
+
+
+    scanf("%d", &x);
+
+
+    if (x == 17) {
+        printf("YAY\n");
+    } else {
+        printf("NO\n");
+    }
+
+    return 0;
+}

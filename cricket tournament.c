edbb247472bfe89bfe27d8c00161cbbe40ef8c7e
Pallavi <pallavishettigar03@gmail.com>
2024-03-06 21:00:28 +0530

    add the question

---
+++ cricket tournament.c
@@ -0,0 +1,34 @@
+// Cricket Tournament
+// Udyam'24 is organizing a cricket tournament, in which 
+// �
+// N teams are participating.
+// This tournament is a knockout tournament, meaning that the loser of each match will be knocked out of the tournament and won't play any more matches.
+
+// The winner of the tournament is whichever team remains in the end without being knocked out.
+
+// The tournament will proceed as follows:
+
+// If there is only one team remaining, that team will be declared the winner, and the tournament finishes.
+// Otherwise, some two teams will play against each other; with the loser being knocked out.
+// You think that the tournament will be interesting if at least 
+// �
+// M matches will be played before the winner is decided.
+// Given 
+// �
+// N and 
+// �
+// M, is it possible for the tournament to be interesting?
+
+// Input Format
+// The first line of input will contain a single integer 
+// �
+// T, denoting the number of test cases.
+// Each test case consists of a single line containing two space-separated integers 
+// �
+// N and 
+// �
+// M — the number of teams and the minimum number of matches for an interesting tournament.
+// Output Format
+// For each test case, output on a new line the answer: "YES" if the tournament can be interesting, and "NO" otherwise (without quotes).
+
+// Each letter of the output may be printed in either uppercase or lowercase, i.e, the strings NO, no, No, and nO will all be treated as equivalent.

9bfdb67705eca70c81a47f3e2a2df43e2e23af91
Pallavi <pallavishettigar03@gmail.com>
2024-03-11 22:46:32 +0530

    add new que

---
+++ aianalyze.c
@@ -0,0 +1,6 @@
+// Input Format
+// The only line of input will contain a single integer C, denoting the number of characters in the code.
+
+// Output Format
+// Output a single line which contains either "Yes", if the feature is available on this code, or "No", if not.
+

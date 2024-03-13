ececd3daecf7798bbdad97e99c1031aa63366d30
Pallavi <pallavishettigar03@gmail.com>
2024-03-13 21:39:26 +0530

    add new changes

---
+++ overspeeding.c
@@ -0,0 +1,9 @@
+// Overspeeding
+// Alice is going for a drive. During her drive, she reached a maximum speed of 
+// S km/hr.
+
+// As per the rules of the government, the speed of the vehicle must not exceed 
+// 40
+// 40 km/hr, otherwise the person will be fined.
+
+// You need to tell whether Alice will be fined or not.

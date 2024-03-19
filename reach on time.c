a56094027c8bfee8d02d2aa342e6d636ea3713bd
Pallavi <pallavishettigar03@gmail.com>
2024-03-19 17:36:51 +0530

    add new changes

--- reach on time.c
+++ reach on time.c
@@ -2,3 +2,20 @@
 // Chef has recently moved into an apartment. It takes 30 minutes for Chef to reach office from the apartment.
 // Chef left for the office X minutes before Chef was supposed to reach. Determine whether or not Chef will be able to reach on time.
 
+#include <stdio.h>
+
+int main() {
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x;
+	    scanf("%d",&x);
+	    if(x>=30){
+	        printf("YES\n");
+	    }
+	    else
+	    printf("NO\n");
+	}
+
+}
+

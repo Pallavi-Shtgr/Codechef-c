6fd21157624a41e7680d3bc64bb768629ac8223c
Pallavi <pallavishettigar03@gmail.com>
2024-03-19 17:39:51 +0530

    add new changes

---
+++ ludo.c
@@ -0,0 +1,4 @@
+// Chef Plays Ludo
+// Chef is playing Ludo. According to the rules of Ludo, a player can enter a new token into the play only when he rolls a 6 on the die.
+
+// In the current turn, Chef rolled the number X on the die. Determine if Chef can enter a new token into the play in the current turn or not.

cf95295dcb7dd35dd65cf07d6b6050f75b8931e5
Pallavi <pallavishettigar03@gmail.com>
2024-03-19 17:35:48 +0530

    add the new changeds

---
+++ reach on time.c
@@ -0,0 +1,4 @@
+// Reach on Time
+// Chef has recently moved into an apartment. It takes 30 minutes for Chef to reach office from the apartment.
+// Chef left for the office X minutes before Chef was supposed to reach. Determine whether or not Chef will be able to reach on time.
+

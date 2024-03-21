23748021a1f7b524515a990e868745bf28a02c07
Pallavi <pallavishettigar03@gmail.com>
2024-03-21 21:14:46 +0530

    add new changes

---
+++ batteryhealth.c
@@ -0,0 +1,12 @@
+// Battery Health
+// Apple considers any iPhone with a battery health of 80% or above, to be in optimal condition.
+// Given that your iPhone has X% battery health, find whether it is in optimal condition.
+
+// Input Format
+// The first line of input will contain a single integer T, denoting the number of test cases.
+// The first and only line of each test case contains an integer X — the battery health.
+
+// Output Format
+// For each test case, output on a new line, YES, if the battery is in optimal condition, and NO otherwise.
+
+// You may print each character in uppercase or lowercase. For example, NO, no, No and nO, are all considered identical.

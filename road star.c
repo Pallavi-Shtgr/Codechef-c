e9f7d1b416eadf99271b58d4ab48c96a22818880
Pallavi <pallavishettigar03@gmail.com>
2024-04-05 20:59:49 +0530

    add new changes

---
+++ road star.c
@@ -0,0 +1,5 @@
+// Reach 5 Star
+// Chef loves giving contests on Codechef. Chef wants to becomes 5 star rated. Currently his rating on Codechef is X.
+// After today's contest, his rating will increase by Y. Note that Y can be negative which means that Chef's rating will decrease.
+// Find whether Chef will become 5 star rated after today's contest.
+// Chef will be considered 5 star rated if his rating is greater than or equal to 2000.

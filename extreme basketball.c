f2d5bf855c9bf4981785c29efae83a93d46e9a56
Pallavi <pallavishettigar03@gmail.com>
2024-04-05 21:10:07 +0530

    add changes

---
+++ extreme basketball.c
@@ -0,0 +1,7 @@
+// Extreme Basketball
+// Alice and Bob are playing a game of basketball against each other, one-versus-one.
+// Currently, Alice has A points, and Bob has B points.
+// Each shot of a player can score either 2 or 3 points.
+// Alice isn't just out to win - she wants to win in dominant fashion.
+// Alice believes that her win is dominant if she ends the game with at least 10 points more than Bob.
+// If Alice is able to prevent Bob from scoring any more points, what's the minimum number of shots she further needs to get a dominant victory?

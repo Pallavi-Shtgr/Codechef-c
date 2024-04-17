9ae7483a5a72f495e7c9d96a849309aa67c7c4ec
Pallavi <pallavishettigar03@gmail.com>
2024-04-17 07:26:27 +0530

    add changes

---
+++ 400m.c
@@ -0,0 +1,5 @@
+// Alice, Bob, and Charlie participated in a 
+// 400
+// 400-metre race.
+// The time taken by Alice, Bob, and Charlie to complete the race was X,Y, and Z seconds respectively. Note that X,Y, and Z are distinct.
+// Determine the person having the highest average speed in the race.

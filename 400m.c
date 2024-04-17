23d81cabd7bd6da4a5bc9956a86fced83d4b201c
Pallavi <pallavishettigar03@gmail.com>
2024-04-17 07:26:55 +0530

    add the sokutions

--- 400m.c
+++ 400m.c
@@ -3,3 +3,32 @@
 // 400-metre race.
 // The time taken by Alice, Bob, and Charlie to complete the race was X,Y, and Z seconds respectively. Note that X,Y, and Z are distinct.
 // Determine the person having the highest average speed in the race.
+#include <stdio.h>
+
+int main(void) 
+{
+	// your code goes here
+	int T,A,B,C;
+	scanf("%d",&T);
+	while(T--)
+	{
+	    scanf("%d %d %d",&A,&B,&C);
+	    if(A!=B && B!=C && C!=A)
+	    {
+	        if(A<B && A<C)
+	        {
+	            printf("\nALICE");
+	        }
+	        else if(B<C)
+	        {
+	            printf("\nBOB");
+	        }
+	        else
+	        {
+	            printf("\nCHARLIE");
+	        }
+	    }
+	}
+	return 0;
+}
+

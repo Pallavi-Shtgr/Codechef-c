05fdc5e01a69610f2ba4e2b659249256224c38a7
Pallavi <pallavishettigar03@gmail.com>
2024-05-13 21:39:01 +0530

    add changes

---
+++ legs on arm.c
@@ -0,0 +1,4 @@
+// Legs on a Farm
+// After a long and fulfilling career, Chef has decided to retire to a farm out in the countryside.
+// This farm has several cows and chickens, but Chef doesn't know exactly how many of each there are — he can only see that there are N legs in total across all the animals.
+// Note that each cow has 4 legs and each chicken has 2 legs, and it is guaranteed that N is even.With N legs in total, what's the minimum possible number of animals that can be present on the farm?

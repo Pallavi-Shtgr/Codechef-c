a3ac59268c3c98863ab083ce6f8ce8125e1e47d9
Pallavi <pallavishettigar03@gmail.com>
2024-05-13 21:39:38 +0530

    Add files via upload

--- legs on arm.c
+++ legs on arm.c
@@ -2,3 +2,24 @@
 // After a long and fulfilling career, Chef has decided to retire to a farm out in the countryside.
 // This farm has several cows and chickens, but Chef doesn't know exactly how many of each there are — he can only see that there are N legs in total across all the animals.
 // Note that each cow has 4 legs and each chicken has 2 legs, and it is guaranteed that N is even.With N legs in total, what's the minimum possible number of animals that can be present on the farm?
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", & T);
+
+    while (T--) {
+        int N;
+        scanf("%d", & N);
+
+        int a;
+        if (N % 4 == 0)
+            a = N / 4;
+        else
+            a = N / 4 + 1;
+
+        printf("%d\n", a);
+    }
+
+    return 0;
+}

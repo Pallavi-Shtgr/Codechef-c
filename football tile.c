4880bda37a87f27354b12c617411aee4fb4ffbd7
Pallavi <pallavishettigar03@gmail.com>
2024-05-22 21:48:47 +0530

    add changes

---
+++ football tile.c
@@ -0,0 +1,65 @@
+
+Football Ties
+According to the Premier League rules, the winner of a game gets 
+3
+3 points while the loser of a game gets 
+0
+0 points. On the other hand, if there is a draw, both sides get 
+1
+1 point each.
+
+Two teams 
+𝐴
+A and 
+𝐵
+B played several games against each other and finally ended up with the score of 
+𝑋
+X and 
+𝑌
+Y points respectively.
+We do not know how many games were played. Find the minimum possible number of draws that may have happened.
+
+It is guaranteed that there is at least one way to end up with the final score being 
+𝑋
+X and 
+𝑌
+Y points respectively.
+
+Input Format
+The first line of input will contain a single integer 
+𝑇
+T, denoting the number of test cases.
+Each test case contains 
+𝑋
+X and 
+𝑌
+Y - the final scores of teams 
+𝐴
+A and 
+𝐵
+B respectively.
+Output Format
+For each test case, output on a new line, the minimum number of draws that could have happened.
+
+Constraints
+1
+≤
+𝑇
+≤
+75
+1≤T≤75
+0
+≤
+𝑋
+,
+𝑌
+≤
+14
+0≤X,Y≤14
+There is at least 
+1
+1 way to obtain the final score 
+𝑋
+X and 
+𝑌
+Y.

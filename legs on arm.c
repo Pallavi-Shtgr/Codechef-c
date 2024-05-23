be828043e2841aa2796d99d6bb66a0009f203181
Pallavi <pallavishettigar03@gmail.com>
2024-05-23 23:46:50 +0530

    add changes

--- legs on arm.c
+++ legs on arm.c
@@ -3,7 +3,6 @@
 // This farm has several cows and chickens, but Chef doesn't know exactly how many of each there are — he can only see that there are N legs in total across all the animals.
 // Note that each cow has 4 legs and each chicken has 2 legs, and it is guaranteed that N is even.With N legs in total, what's the minimum possible number of animals that can be present on the farm?
 #include <stdio.h>
-
 int main() {
     int T;
     scanf("%d", & T);
--- money double.c
+++ money double.c
@@ -2,16 +2,13 @@
 // Money Double
 // Chef's bank gives him a unique offer — at the end of each year, they offer Chef to either add 1000 rupees to his bank account, or double the amount stored in his bank account.
 // Chef initially has X rupees in his account. What is the maximum amount of money that Chef can accumulate after Y years?
-
 // Input Format
 // The first line of input will contain a single integer T, denoting the number of test cases.
 // The first and only line of each test case contains 2 space-separated integers X and Y, as described in the statement.
-
 // Output Format
 // For each test case, output on a new line one integer: the maximum amount of money which Chef can accumulate after Y years.
 
 #include<stdio.h>
-
     int M(int X, int Y) {
     for (int i = 0; i < Y; ++i) {
         if (X * 2 > X + 1000) {

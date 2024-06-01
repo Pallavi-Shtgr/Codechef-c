c19b4ecc75dc4655e54f9828df9fa294fed1bd38
Pallavi <pallavishettigar03@gmail.com>
2024-06-01 23:45:42 +0530

    add changes

---
+++ even sum.c
@@ -0,0 +1,6 @@
+// Even Sum Subarray
+// Given an array A of size N, find the size of the largest subarray of 𝐴
+// A which has an even sum.For example, for the array 
+// [2,1,2][2,1,2] the answer is just 11 as the largest subarray with an even sum is [2][2].
+// Note that a subarray is a contiguous part of an array. For the array 
+// [1,3,2][1,3,2], some possible subarrays are [1],[2],[1,3],[1,3,2[1],[2],[1,3],[1,3,2]. However,[1,2] [1,2] is not a subarray for this array.

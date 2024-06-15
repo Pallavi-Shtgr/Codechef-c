a7491fb6d02c9880eb4bb62c014dd4d34e5bbfaf
Pallavi <pallavishettigar03@gmail.com>
2024-06-15 19:28:31 +0530

    add changes

---
+++ long drive.c
@@ -0,0 +1,3 @@
+// Long Drive
+// Chef and Chefina are out on a long road trip.The average speed of the car after 10 hours of driving is X kilometres per hour.
+// Chef wants to achieve an average speed of Y kilometres per hour. Given that he can travel a maximum of 100 kilometres in one hour, find the minimum number of additional integer hours required for him to reach the target average speed.

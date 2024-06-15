28dd81b0c8109f1c8c2c395d99405af33e3f71c0
Pallavi <pallavishettigar03@gmail.com>
2024-06-15 18:43:38 +0530

    add changes

---
+++ heat wave.c
@@ -0,0 +1,5 @@
+// Heat Wave
+// During a scorching heat wave, the temperature in a Chefland reached a record high of X degrees.
+// The next day, the recorded temperature was Y degrees. Find whether this was a new record high or not.
+// Input Format
+// The first and only line of input will contain two space separated integers X and Y denoting the highest recorded temperature and the temperature on a given day respectively.

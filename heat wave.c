a61b8be447924a578c2e73f760081b65257f681d
Pallavi <pallavishettigar03@gmail.com>
2024-06-15 18:45:13 +0530

    add changes

--- heat wave.c
+++ heat wave.c
@@ -3,3 +3,17 @@
 // The next day, the recorded temperature was Y degrees. Find whether this was a new record high or not.
 // Input Format
 // The first and only line of input will contain two space separated integers X and Y denoting the highest recorded temperature and the temperature on a given day respectively.
+
+#include <stdio.h>
+int main(void) {
+    int x,y;
+    scanf("%d%d",&x,&y);
+     if (y > x) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+
+    return 0;
+
+}

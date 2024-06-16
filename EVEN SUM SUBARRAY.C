80c77beefd20bb35f70f6524c75516c8c6e85c7f
Pallavi <pallavishettigar03@gmail.com>
2024-06-16 13:18:41 +0530

    add changes

---
+++ EVEN SUM SUBARRAY.C
@@ -0,0 +1,10 @@
+// Input Format
+// The first line of input will contain a single integer 
+// 𝑇
+// T, denoting the number of test cases.
+// Each test case consists of multiple lines of input.
+// The first line of each test case contains a single integer 
+// 𝑁
+// N — the size of the array.
+// Output Format
+// For each test case, output on a new line, the size of the largest subarray with even sum.

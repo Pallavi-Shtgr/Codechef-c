eab41ee4d0acd8969774cffb2e3045db156ebf05
Pallavi <pallavishettigar03@gmail.com>
2024-06-26 23:01:47 +0530

    add changes

---
+++ yoga.c
@@ -0,0 +1,8 @@
+// On the occasion of Yoga Day, the only yoga instructor in Chefland, Chef, has received numerous queries.
+// Chef has managed to free up 
+// 𝑁
+// N hours from his busy schedule to conduct yoga sessions. There are two types of sessions that Chef offers:
+// Type 1 session, which lasts 1 hour, and earns Chef X rupees.
+// Type 2 session, which lasts 2 hours, and earns Chef  (Y>X) rupees.
+
+// Find the maximum amount of money Chef can earn in N hours.

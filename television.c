d328031633bcd6d25e4c3f8b4791e374eda60198
Pallavi <pallavishettigar03@gmail.com>
2024-07-02 21:28:01 +0530

    Add changes

---
+++ television.c
@@ -0,0 +1,6 @@
+// Television Channels
+// You have a television which had 
+// 𝑋
+// X working channels numbered 1,2,...,X. Unfortunately, due to a recent change, all even-numbered channels stopped working.
+// For example if X=5, channels numbered 2 and 4 have stopped working , while channels numbered 3 and 5 are still working.
+// Given X, can you tell how many channels are still working?

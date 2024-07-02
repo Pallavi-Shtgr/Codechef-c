431411f7ec2d328efac89ba97b1b8d9b0857d4d6
Pallavi <pallavishettigar03@gmail.com>
2024-07-02 21:28:38 +0530

    Add solution

--- television.c
+++ television.c
@@ -4,3 +4,16 @@
 // X working channels numbered 1,2,...,X. Unfortunately, due to a recent change, all even-numbered channels stopped working.
 // For example if X=5, channels numbered 2 and 4 have stopped working , while channels numbered 3 and 5 are still working.
 // Given X, can you tell how many channels are still working?
+
+#include <stdio.h>
+
+int main(void) {
+    int X;
+    scanf("%d", &X);
+    
+    int w = (X + 1) / 2;
+    
+    printf("%d\n", w);
+    
+    return 0;
+}

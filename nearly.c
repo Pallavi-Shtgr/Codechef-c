8bd1e8fe82102ef9083e67b4b84d9c72a03c6b27
Pallavi <pallavishettigar03@gmail.com>
2024-07-03 21:05:20 +0530

    Add changes

---
+++ nearly.c
@@ -0,0 +1,41 @@
+// Nearly Equal
+// The Hamming distance between two pairs of strings of equal length is defined to be the number of positions at which they contain different characters.
+// For example, the Hamming distance between strings 
+// "there"
+// "there" and 
+// "shire"
+// "shire" is 
+// 2
+// 2 (their first and third characters are different), while the Hamming distance between 
+// "order"
+// "order" and 
+// "chaos"
+// "chaos" is 
+// 5
+// 5, since they differ at every position.
+
+// Chef has a string 
+// 𝐴
+// A of length 
+// 𝑁
+// N.
+// Chef's favorite string is 
+// 𝐵
+// B, which has length 
+// 𝑀
+// M. It is known that 
+// 𝑀
+// ≤
+// 𝑁
+// M≤N.
+
+// Find the minimum Hamming distance between 
+// 𝐵
+// B and some contiguous substring
+// †
+// †
+//   of 
+// 𝐴
+// A that has length 
+// 𝑀
+// M.

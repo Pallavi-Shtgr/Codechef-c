b28bbd849a1c298cb2e5fe72142764116cb182af
Pallavi <pallavishettigar03@gmail.com>
2024-07-27 21:56:41 +0530

    Add chnges

---
+++ pending assignments.c
@@ -0,0 +1,3 @@
+// Chef has finally decided to complete all of his pending assignments.
+// There are X assignments where each assignment takes Y minutes to complete.
+// Find whether Chef would be able to complete all the assignments in Z days.

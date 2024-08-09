98f1220b72675539efae395eee3d0c2a7cec6f59
Pallavi <pallavishettigar03@gmail.com>
2024-08-09 18:32:41 +0530

    add changes

--- 2000.c
+++ 2000.c
@@ -1,2 +1,10 @@
 // Chef had collected N notes of Rs. 2000 to pay his total college fees. However, the government banned Rs. 2000 notes.
 // Chef wants to pay the same amount using Rs. 500 notes only. Find the number of notes Chef needs.
+
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d",&n);
+	printf("%d",(n*2000)/500);
+	}

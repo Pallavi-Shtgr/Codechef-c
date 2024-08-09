c84f6890e207c458a5c3518f7d3f92ed93523c0a
Pallavi <pallavishettigar03@gmail.com>
2024-08-09 18:32:01 +0530

    Add changes

---
+++ 2000.c
@@ -0,0 +1,2 @@
+// Chef had collected N notes of Rs. 2000 to pay his total college fees. However, the government banned Rs. 2000 notes.
+// Chef wants to pay the same amount using Rs. 500 notes only. Find the number of notes Chef needs.

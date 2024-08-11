ee857e3684de65650b5f330c7f42755e3fd48564
Pallavi <pallavishettigar03@gmail.com>
2024-08-11 08:41:28 +0530

    add changes

--- election in checgland.c
+++ election in checgland.c
@@ -1,42 +1,6 @@
-// Elections in Chefland
-// There are 
-// 101
-// 101 citizens in Chefland. It is election time in Chefland and 
-// 3
-// 3 parties, 
-// A
-// ,
-// B
-// A,B, and 
-// C
-// C are contesting the elections. Party 
-// A
-// A receives 
-// X
-// A
-// X 
-// A
-// ​
-//   votes, party 
-// B
-// B receives 
-// X
-// B
-// X 
-// B
-// ​
-//   votes, and party 
-// C
-// C receives 
-// X
-// C
-// X 
-// C
-// ​
-//   votes.
-
+// Elections in Chefland There are 101 citizens in Chefland. It is election time in Chefland and 3 parties, 
+// A,B and C are contesting the elections. Party 
+// A receives X votes, party X B votes, and party C receives votes.
 // The constitution of Chefland requires a particular party to receive a clear majority to form the government. A party is said to have a clear majority if it receives strictly greater than 
-// 50
 // 50 votes.
-
 // If any party has a clear majority, print the winning party (A, B or C). Otherwise, print NOTA.

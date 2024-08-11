611395224b41f6c6e12bf0f85a9f9818ddba4999
Pallavi <pallavishettigar03@gmail.com>
2024-08-11 08:41:59 +0530

    add changes

--- election in checgland.c
+++ election in checgland.c
@@ -4,3 +4,45 @@
 // The constitution of Chefland requires a particular party to receive a clear majority to form the government. A party is said to have a clear majority if it receives strictly greater than 
 // 50 votes.
 // If any party has a clear majority, print the winning party (A, B or C). Otherwise, print NOTA.
+
+#include<stdio.h>
+
+int main()
+
+{
+    int a,b,c,t;
+
+    scanf("%d",&t);
+
+    while(t)
+
+    {
+        scanf("%d %d %d",&a,&b,&c);
+
+        if ( a>50 )
+
+        {
+            printf("A\n");
+        }
+
+        else if ( b>50 )
+
+        {
+            printf("B\n");
+        }
+
+        else if ( c>50 )
+
+        {
+            printf("C\n");
+        }
+
+        
+        else printf("NOTA\n");
+
+
+        t--;
+    }
+
+    return 0;
+}

48e44dddb7ab192aa7e6ceab8a8650b343aec919
Pallavi <pallavishettigar03@gmail.com>
2024-08-16 18:00:53 +0530

    add chanes

--- gold coin.c
+++ gold coin.c
@@ -2,3 +2,17 @@
 // It is known that the winner of the game receives A gold coins while the loser receives B gold coins.
 // Given that Chef scored X goals and Chefina scored Y goals (X≠Y)
 // (X=Y), find the number of coins Chef would receive.
+
+#include <stdio.h>
+
+int main(void) {
+     int a,b,x,y;
+     scanf("%d%d%d%d",&a,&b,&x,&y);
+     if (x>y)
+     printf("%d",a);
+     else 
+     printf("%d",b);
+     
+
+}
+

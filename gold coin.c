97f090cbb4e58172b6acc869cc49859024c78aa6
Pallavi <pallavishettigar03@gmail.com>
2024-08-16 18:00:23 +0530

    add chnages

---
+++ gold coin.c
@@ -0,0 +1,4 @@
+// Chef and Chefina are competing against each other in a football game where the player scoring the maximum goals, wins.
+// It is known that the winner of the game receives A gold coins while the loser receives B gold coins.
+// Given that Chef scored X goals and Chefina scored Y goals (X≠Y)
+// (X=Y), find the number of coins Chef would receive.

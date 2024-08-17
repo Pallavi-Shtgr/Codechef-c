61f9c7e6b61c981757e762ead08ca9bfa1d4a7a4
Pallavi <pallavishettigar03@gmail.com>
2024-08-17 20:32:02 +0530

    add changes

---
+++ slow solution.c
@@ -0,0 +1,15 @@
+// Slow Solution
+// Chef is trying to solve a problem having 
+// T
+// T test cases, where, for each test case he is given a single integer 
+// N
+// N.
+
+// Chef has an algorithm which takes exactly 
+// N
+// 2
+// N 
+// 2
+//   iterations for a test case with value 
+// N
+// N.

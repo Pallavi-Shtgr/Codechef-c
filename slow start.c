28e180e205d73f151c72a5df93ccd32a9a8e8323
Pallavi <pallavishettigar03@gmail.com>
2024-08-19 08:31:52 +0530

    add changes

--- slow start.c
+++ slow start.c
@@ -2,3 +2,30 @@
 // Chef's favorite Monster has an attack stat of X, and is fighting against another monster whose health stat is H. The fight proceeds in turns: each turn, Chef's Monster does X damage to its opponent.However, Chef's Monster has the ability Slow Start, which halves its attack for the first five turns.
 // That is, for the first five turns, the damage done to the opponent is 
 // X2 X and not X.
+
+#include <stdio.h>
+
+int main() {
+    int T; // Number of test cases
+    scanf("%d", &T);
+
+    while (T--) {
+        int X, H; 
+        scanf("%d %d", &X, &H);
+
+      
+        int turns = 0;
+        while (H > 0) {
+            if (turns < 5) {
+                H -= X / 2;
+            } else {
+                H -= X;
+            }
+            turns++;
+        }
+
+        printf("%d\n", turns);
+    }
+
+    return 0;
+}

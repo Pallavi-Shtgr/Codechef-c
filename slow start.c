fae995467c568b9f400ef6d2e5845344c732c8c7
Pallavi <pallavishettigar03@gmail.com>
2024-08-19 08:30:40 +0530

    add changes

---
+++ slow start.c
@@ -0,0 +1,4 @@
+// Chef is playing a certain video game that requires him to collect Monsters, and use them to fight against other Monsters.
+// Chef's favorite Monster has an attack stat of X, and is fighting against another monster whose health stat is H. The fight proceeds in turns: each turn, Chef's Monster does X damage to its opponent.However, Chef's Monster has the ability Slow Start, which halves its attack for the first five turns.
+// That is, for the first five turns, the damage done to the opponent is 
+// X2 X and not X.

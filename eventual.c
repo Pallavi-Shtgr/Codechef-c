49c14b11a3d09993f802bb35775c13305299a839
Pallavi <pallavishettigar03@gmail.com>
2024-08-21 08:37:55 +0530

    add changesZ

---
+++ eventual.c
@@ -0,0 +1,15 @@
+// You are given a string 
+// S
+// S with length 
+// N
+// N. You may perform the following operation any number of times: choose a non-empty substring of 
+// S
+// S (possibly the whole string 
+// S
+// S) such that each character occurs an even number of times in this substring and erase this substring from 
+// S
+// S. (The parts of 
+// S
+// S before and after the erased substring are concatenated and the next operation is performed on this shorter string.)
+
+// For example, from the string "acabbad", we can erase the highlighted substring "abba", since each character occurs an even number of times in this substring. After this operation, the remaining string is "acd".

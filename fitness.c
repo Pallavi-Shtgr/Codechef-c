c6a9fbe8d07b7cd16e23dad7ac45d2e02013f8d9
Pallavi <pallavishettigar03@gmail.com>
2024-08-21 08:20:23 +0530

    add changes

---
+++ fitness.c
@@ -0,0 +1,3 @@
+// Chef wants to become fit for which he decided to walk to the office and return home by walking.
+// It is known that Chef's office is X km away from his home.
+// If his office is open on 5 days in a week, find the number of kilometres Chef travels through office trips in a week.

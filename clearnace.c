5af2c113a9385b9f32419b498bc7d91bce31b759
Pallavi <pallavishettigar03@gmail.com>
2024-08-22 21:02:24 +0530

    add cchanges

---
+++ clearnace.c
@@ -0,0 +1,4 @@
+// Chef's favorite clothing store is holding a clearance sale: for every 2
+// 2 t-shirts bought, you receive a third one for free! Chef bought and paid for  X t-shirts from the shop. It is guaranteed that  X is even.
+// How many t-shirts did Chef get from the shop in total?
+

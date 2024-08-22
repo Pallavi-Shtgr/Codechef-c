2a9107682b7d834b8026046c7fb29eec4ebabfc9
Pallavi <pallavishettigar03@gmail.com>
2024-08-22 21:02:52 +0530

    add changes

--- clearnace.c
+++ clearnace.c
@@ -2,3 +2,12 @@
 // 2 t-shirts bought, you receive a third one for free! Chef bought and paid for  X t-shirts from the shop. It is guaranteed that  X is even.
 // How many t-shirts did Chef get from the shop in total?
 
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int x;
+	scanf("%d",&x);
+	int m= x/2;
+	printf("%d",x+m);
+}

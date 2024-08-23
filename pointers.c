b8218503a401d2af7a989d029938ee3f1692a503
Pallavi <pallavishettigar03@gmail.com>
2024-08-23 20:46:17 +0530

    Add changes

---
+++ pointers.c
@@ -0,0 +1,4 @@
+// Pointers - Debug this code
+// The code in the IDE is trying to write a program that takes an integer as input, increments its value by 1 using a function, and then prints the updated value.
+// The code is trying to use the concept of pointers - however, it is incorrect.
+// Debug the code to solve this problem.

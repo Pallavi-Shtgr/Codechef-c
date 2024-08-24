4f8821708b5edc6d5b9160983f50f8510dab91be
Pallavi <pallavishettigar03@gmail.com>
2024-08-24 08:51:12 +0530

    add changes

---
+++ count loop.c
@@ -0,0 +1,6 @@
+// Given a linked list, there may exist a single loop or no loop. If there are a total of X elements present in a loop, return X-1 as your answer; otherwise, in case no loop exists, return -1.
+// To solve this problem, complete the function present in the submit solution tab.
+// Input Format
+// The first line will contain N, representing the elements in the linked list
+// Constraints
+// Maximum elements in the linked list can be 10^5.

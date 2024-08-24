8405eef2693826503898e7189169dcc10f52a087
Pallavi <pallavishettigar03@gmail.com>
2024-08-24 08:52:02 +0530

    add changes

--- count loop.c
+++ count loop.c
@@ -4,3 +4,27 @@
 // The first line will contain N, representing the elements in the linked list
 // Constraints
 // Maximum elements in the linked list can be 10^5.
+
+class Solution{
+    static int findlength(Node root,Node slow,Node fast){
+        int count=1;
+        fast=fast.next;
+        while(fast!=slow){
+            fast=fast.next;
+            count++;
+        }
+        return count;
+    }
+    static int solve(Node root){
+       Node slow=root;
+       Node fast=root;
+       while(fast!=null && fast.next!=null){
+           slow=slow.next;
+           fast=fast.next.next;
+           if(slow==fast){
+               return findlength(root,slow,fast);
+           }
+       }
+       return -1;
+    }
+}

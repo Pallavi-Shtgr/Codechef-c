e16cabcde408fdfdca236a405d176a62f3fe4770
Pallavi <126679884+Pallavi-Shtgr@users.noreply.github.com>
2024-08-25 11:53:50 +0530

    Update cakehalf.c

--- cakehalf.c
+++ cakehalf.c
@@ -1,8 +1,8 @@
-Let Me Eat Cake!
-Alice and Bob both bought a full cake each.
-Alice's cake has A slices, and Bob's cake has B slices.
-Alice will be unhappy if she has strictly less slices than Bob does. Similarly, Bob will be unhappy if he has strictly less slices than Alice does.
-Charlie doesn't want either of his friends to be unhappy, so he proposes a solution: while either Alice or Bob are unhappy, he will attempt to restore happiness by eating half the remaining slices (rounded up) of whoever has more of them.
+// Let Me Eat Cake!
+// Alice and Bob both bought a full cake each.
+// Alice's cake has A slices, and Bob's cake has B slices.
+// Alice will be unhappy if she has strictly less slices than Bob does. Similarly, Bob will be unhappy if he has strictly less slices than Alice does.
+// Charlie doesn't want either of his friends to be unhappy, so he proposes a solution: while either Alice or Bob are unhappy, he will attempt to restore happiness by eating half the remaining slices (rounded up) of whoever has more of them.
 
   #include <stdio.h>
 

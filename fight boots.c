90f27f5b1355bbe31d2e88359534b7fc87696c2c
Pallavi <pallavishettigar03@gmail.com>
2024-08-26 19:29:51 +0530

    add changes

--- fight boots.c
+++ fight boots.c
@@ -1,32 +1,5 @@
 // FightBots
-// Alice and Bob are competing in the finals of a FightBots tournament.
-
-// The match takes place on the 
-// 2
-// 2-D plane.
-// Alice's robot starts at position 
-// (
-// 0
-// ,
-// 0
-// )
-// (0,0), while Bob's starts at position 
-// (
-// X
-// ,
-// Y
-// )
-// (X,Y).
-// It is guaranteed that 
-// (
-// X
-// ,
-// Y
-// )
-// ≠
-// (
-// 0
-// ,
-// 0
-// )
+// Alice and Bob are competing in the finals of a FightBots tournament.The match takes place on the 2-D plane.
+// Alice's robot starts at position (0,0)
+// (0,0), while Bob's starts at position (X,Y). It is guaranteed that (X,Y)≠(0,0)
 // (X,Y)=(0,0).

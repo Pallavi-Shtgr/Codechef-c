1192c193e70c5daaeebc6f7190c45e036203943c
Pallavi <pallavishettigar03@gmail.com>
2024-08-27 20:51:14 +0530

    add changes

--- restropay.c
+++ restropay.c
@@ -8,3 +8,41 @@
 
 // Output Format
 // YES if it is possible to pay the bill using just 2 banknotes ( one from Purgi and one from Tanny). Else output NO.
+
+#include <stdio.h>
+
+int main(void) {
+    long long int sum;
+    int i,j,size1,size2,flag=2;
+    scanf("%d %d %lld",&size1,&size2,&sum);
+    long long int a[size1],b[size2];
+    for(i=0;i<size1;i++)
+    {
+        scanf("%lld",&a[i]);
+    }
+    for(i=0;i<size2;i++)
+    {
+        scanf("%lld",&b[i]);
+    }
+    for(i=0;i<size1;i++)
+    {
+        if(a[i]<sum)
+        {
+            for(j=0;j<size2;j++)
+            {
+                if(sum-a[i]==b[j])
+                {
+                    flag=1;
+                }
+            }
+        }
+    }
+    if(flag==1)
+    printf("YES\n");
+    else 
+    printf("NO\n");
+	// your code goes here
+	return 0;
+}
+
+

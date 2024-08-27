57efefc3262e111d96cb02c6c789525da746a1b2
Pallavi <pallavishettigar03@gmail.com>
2024-08-27 20:50:25 +0530

    Add changes

---
+++ restropay.c
@@ -0,0 +1,10 @@
+// RestroPay
+
+// Tanny and Purgi are two very good friends. They went to a nice restaurant for some good dinner after a long taxing week. 
+// Input Format
+// The first line will have 3 integers n,mc
+// n,m,c separated by a space.
+// The second and third line will contain the values of the banknotes Tanny and Purgi (separated by a space) respectively.
+
+// Output Format
+// YES if it is possible to pay the bill using just 2 banknotes ( one from Purgi and one from Tanny). Else output NO.

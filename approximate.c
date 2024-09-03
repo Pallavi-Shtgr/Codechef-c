e7fe11364a1528fa728990c24b02a79adf692d3f
Pallavi <pallavishettigar03@gmail.com>
2024-09-03 19:47:12 +0530

    Add changes

--- approximate.c
+++ approximate.c
@@ -2,3 +2,18 @@
 // The answer you obtained, however, is the integer X.
 // Your answer will be considered correct if the difference between X and Y is at most K.
 // In other words, your answer is considered correct if and only if:
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+    int x,y,z;
+    scanf("%d%d%d",&x,&y,&z);
+    if(abs(x-y)<=z){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+}
+

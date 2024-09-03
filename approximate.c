ffe1241c6847cc23cda8d4c7e9916bf3a6ecfdc4
Pallavi <pallavishettigar03@gmail.com>
2024-09-03 19:46:30 +0530

    Add que

---
+++ approximate.c
@@ -0,0 +1,4 @@
+// You are solving a problem whose correct answer is the integer Y.
+// The answer you obtained, however, is the integer X.
+// Your answer will be considered correct if the difference between X and Y is at most K.
+// In other words, your answer is considered correct if and only if:

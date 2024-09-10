e45b3aed4dac2a3c05499d09d3c323a0b437f930
Pallavi <pallavishettigar03@gmail.com>
2024-09-10 17:41:57 +0530

    add changes

---
+++ magical world.c
@@ -0,0 +1,5 @@
+// Input Format
+// The first line of input will contain a single integer T, denoting the number of test cases.
+// The first and only line of each test case will contain three space-separated integers A, B, and X — the length of the red rectangle, the width of the red rectangle, and the length of the edge of the blue square.
+// Output Format
+// For each test case, output on a new line the minimum cost required to make the area of the blue square greater than or equal to the area of the red rectangle

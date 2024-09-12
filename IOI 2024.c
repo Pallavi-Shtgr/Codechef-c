a6e63e08335e79bf60ce99ee70fc57a1b47cbbfe
Pallavi <126679884+Pallavi-Shtgr@users.noreply.github.com>
2024-09-12 21:04:29 +0530

    Update IOI 2024.c

--- IOI 2024.c
+++ IOI 2024.c
@@ -1,1 +1,9 @@
-
+// IOI 2024 is being held in Egypt, starting from the 
+// 1-st of September and ending on the 8-th of September.
+// Given a date 
+// X in September, output YES if IOI is ongoing then, otherwise NO.
+// Input Format
+// The first and only line of input contains one integer X - the date in September.
+// Output Format
+// Output either YES or NO, depending on whether IOI is ongoing on September X.
+// Each character of the output may be printed in either lowercase or uppercase - that is, the strings no, NO, No, and nO will all be treated as equivalent.

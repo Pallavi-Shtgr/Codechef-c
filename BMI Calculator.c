204d8494530abd3f42e093c45a4b7bb1a59d224f
Pallavi <126679884+Pallavi-Shtgr@users.noreply.github.com>
2024-11-24 19:12:26 +0530

    Update BMI Calculator.c

--- BMI Calculator.c
+++ BMI Calculator.c
@@ -1,1 +1,13 @@
+// Given the height (1.82 meter) and weight (72 kg) of the Chef, calculate his BMI (Body Mass Index).
+// Formula to calculate the BMI
+
+#include <stdio.h>
+
+int main() {
+    float height = 1.82;
+    float weight = 72;
+    float bmi = weight / (height * height);
+    printf("%f", bmi);
+    return 0;
+}
 

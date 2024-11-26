bde5b1ec9628de85e9eedf126c8d83826f287b8a
Pallavi <126679884+Pallavi-Shtgr@users.noreply.github.com>
2024-11-26 07:24:57 +0530

    Update Bootdatatype.c

--- Bootdatatype.c
+++ Bootdatatype.c
@@ -1,1 +1,18 @@
+// In programming, you often need to know if something is true or false.
+// For this, we can have variables containing just one of these two values - True or False.
+// When the value is returned or printed, true = 1 and false = 0.
+// These variables are called "bool" or "boolean variables".
+// We will look at applications of these in later modules.
+// The header stdbool.h in the C Standard Library for the C programming language contains four macros for a Boolean data type.
 
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+  bool x = true;
+  bool y = false;
+  printf("%d\n", x);
+  printf("%d", y);  
+  
+}

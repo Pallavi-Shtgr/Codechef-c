8d0139cbd0e15726110209098b14fb796e593f65
Pallavi <126679884+Pallavi-Shtgr@users.noreply.github.com>
2024-12-06 22:38:00 +0530

    Update Exams.c

--- Exams.c
+++ Exams.c
@@ -1,1 +1,7 @@
-
+In Chefland, there are X schools, and each school has Y students.
+The year end results are in and a total of Z students passed the exams.
+Assuming that all students appeared for the exams, find whether the number of students who passed in Chefland was strictly greater than 50%.
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of three space-separated integers 
+X,Y and Z as mentioned in the statement.

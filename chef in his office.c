de7b37d13acbe791224471b4d6da1637840be7ed
Pallavi <126679884+Pallavi-Shtgr@users.noreply.github.com>
2024-12-08 18:26:53 +0530

    Update chef in his office.c

--- chef in his office.c
+++ chef in his office.c
@@ -1,1 +1,3 @@
-
+// Recently Chef joined a new company. In this company, the employees have to work for X hours each day from Monday to Thursday. Also, in this company, Friday is called Chill Day — employees only have to work for Y hours 
+// (Y<X) on Friday. Saturdays and Sundays are holidays.
+// Determine the total number of working hours in one week.
